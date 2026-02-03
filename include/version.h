65d8e405e6659de16cb520238ade27776055d46f
Veramacor <jwein@HPMINI>
2026-02-03 16:00:22 -0500

    Disable card code voxel transmissions: comment out c1/c2/c3 code strings in high-low game for now

--- include/version.h
+++ include/version.h
@@ -3,8 +3,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define ESP32MUD_VERSION "26.02.02"
-#define COMPILE_DATE "Feb 02 2026"
-#define COMPILE_TIME "20:44:26"
+#define ESP32MUD_VERSION "26.02.03"
+#define COMPILE_DATE "Feb 03 2026"
+#define COMPILE_TIME "15:59:52"
 
 #endif // VERSION_H
